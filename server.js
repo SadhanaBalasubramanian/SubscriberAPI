a528a878883bad095a607a841210a3f8a5bffdef
SadhanaBalasubramanian <sadhanabalu86@gmail.com>
2021-04-24 23:23:21 +0530

    added subscriber model and rest methods for GET and POST

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
+require ("dotenv").config();
+
 const express =require("express");
 const app=new express();
 const mongoose=require("mongoose");
 
-mongoose.connect("mongodb://localhost/subscribers",{useNewUrlParser:true});
+const run = async () => {
+     await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    };
+
+run().catch(error => console.error(error));
+
 const db=mongoose.connection;
 
 db.on("error",(error)=>console.error(error));
 db.once("open",()=>console.log("DB Connected"));
+
+app.use(express.json())
+
+const subscriberRouter=require("./routes/subscribers");
+app.use("/subscribers",subscriberRouter)
 
 app.listen(3000,()=>console.log("Server Started"));
